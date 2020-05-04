489dff84937998fec4b0908fcd14aeb3309f5355
SacleuxBenoit <benoitsacleux@outlook.fr>
2020-05-04 18:24:51 +0200

    add a title with a navbar in the component Header.vue

---
+++ front/src/components/Header.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-card class="overflow-hidden">
+    <div class="center HeaderColor">
+      <v-toolbar-title>Template vuetify</v-toolbar-title> 
+     <v-btn>
+      <router-link to="/"><strong>Home</strong></router-link>
+    </v-btn>
+    <v-btn>
+ <router-link to="#"><strong>Button</strong></router-link> 
+    </v-btn>
+    <v-btn>
+ <router-link to="#"><strong>?</strong></router-link>
+    </v-btn>
+
+
+  </div>
+  </v-card>
+</template>
+
+<style>
+.center{
+    text-align: center;
+}
+.HeaderColor{
+   background-color:#eae9e9
+}
+</style>
