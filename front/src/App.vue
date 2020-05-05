1226084326db5ade4f09f2a26965dea6690dcc0b
SacleuxBenoit <benoitsacleux@outlook.fr>
2020-05-05 17:41:48 +0200

    add the import of the component Header in App.vue

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,12 +1,17 @@
 <template>
   <div>
+       <Header/>
     <router-view />
+ 
   </div>
 </template>
 
 <script>
-
+import Header from './components/Header'
 export default {
   name: "App",
+  components:{
+    Header
+  }
 };
 </script>
