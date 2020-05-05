fe813a60528772322600b2250e5214505d386b56
SacleuxBenoit <benoitsacleux@outlook.fr>
2020-05-05 17:29:33 +0200

    import the component Button in the views Button.md

--- front/src/views/Button.vue
+++ front/src/views/Button.vue
@@ -1,13 +1,15 @@
 <template>
   <div class="button">
+      <Button/>
   </div>
 </template>
 
 <script>
+import Button from '../components/Button'
 export default {
   name: "button",
   components: {
-    
+    Button
   }
 };
 </script>
