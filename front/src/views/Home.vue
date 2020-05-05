971a1d7115e13e6bd662104dc7f75c797d7092dc
SacleuxBenoit <benoitsacleux@outlook.fr>
2020-05-05 17:40:30 +0200

    delete the import of the component Header in views/Home (I'll add it to App.vue)

--- front/src/views/Home.vue
+++ front/src/views/Home.vue
@@ -1,15 +1,12 @@
 <template>
   <div class="home">
-<Header/>
   </div>
 </template>
 
 <script>
-import Header from '../components/Header'
 export default {
   name: "Home",
   components: {
-    Header
   }
 };
 </script>
