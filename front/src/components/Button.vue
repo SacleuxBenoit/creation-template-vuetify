2fea3bf61b10aa0ea22b7cd3aadf4983d8522a73
SacleuxBenoit <benoitsacleux@outlook.fr>
2020-05-06 19:52:05 +0200

    add some Button in the component Button.vue

--- front/src/components/Button.vue
+++ front/src/components/Button.vue
@@ -1,9 +1,48 @@
 <template>
-    <div>
-        
-    </div>
-</template>
+  <v-row align="center">
+    <v-col class="text-center" cols="12" sm="3">
+      <div class="my-2">
+        <v-btn depressed small>Normal</v-btn>
+      </div>
+      <div class="my-2">
+        <v-btn depressed small>Primary</v-btn>
+      </div>
+      <div class="my-2">
+        <v-btn depressed small>Error</v-btn>
+      </div>
+      <div class="my-2">
+        <v-btn depressed small disabled>Disabled</v-btn>
+      </div>
+    </v-col>
 
-<style>
-    
-</style>
+    <v-col class="text-center" cols="12" sm="3">
+      <div class="my-2">
+        <v-btn depressed>Normal</v-btn>
+      </div>
+      <div class="my-2">
+        <v-btn depressed>Primary</v-btn>
+      </div>
+      <div class="my-2">
+        <v-btn depressed>Error</v-btn>
+      </div>
+      <div class="my-2">
+        <v-btn depressed disabled>Disabled</v-btn>
+      </div>
+    </v-col>
+
+    <v-col class="text-center" cols="12" sm="3">
+      <div class="my-2">
+        <v-btn depressed large>Normal</v-btn>
+      </div>
+      <div class="my-2">
+        <v-btn depressed large>Primary</v-btn>
+      </div>
+      <div class="my-2">
+        <v-btn depressed large>Error</v-btn>
+      </div>
+      <div class="my-2">
+        <v-btn depressed large disabled>Disabled</v-btn>
+      </div>
+    </v-col>
+  </v-row>
+</template>
