1560467fe889fe1f621dcac1fca84ed66592ab7a
SacleuxBenoit <benoitsacleux@outlook.fr>
2020-05-07 14:32:46 +0200

    import the component Icones.vue to views/Button.vue

--- front/src/views/Button.vue
+++ front/src/views/Button.vue
@@ -1,15 +1,18 @@
 <template>
   <div class="button">
       <Button/>
+      <Icones/>
   </div>
 </template>
 
 <script>
 import Button from '../components/Button'
+import Icones from '../components/Icones'
 export default {
   name: "button",
   components: {
-    Button
+    Button,
+    Icones
   }
 };
 </script>
