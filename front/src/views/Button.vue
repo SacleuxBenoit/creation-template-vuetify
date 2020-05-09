204377287a8d28f1c90939967849a6a135d652ff
SacleuxBenoit <benoitsacleux@outlook.fr>
2020-05-09 17:35:52 +0200

    import the component ButtonSizing in views/Button.vue

--- front/src/views/Button.vue
+++ front/src/views/Button.vue
@@ -1,17 +1,20 @@
 <template>
   <div class="button">
       <Button/>
+      <ButtonSizing/>
       <Icones/>
   </div>
 </template>
 
 <script>
 import Button from '../components/Button/Button.vue'
+import ButtonSizing from '../components/Button/ButtonSizing.vue'
 import Icones from '../components/Button/Icones'
 export default {
   name: "button",
   components: {
     Button,
+    ButtonSizing,
     Icones
   }
 };
