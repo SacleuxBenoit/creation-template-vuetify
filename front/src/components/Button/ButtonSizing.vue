24c04e528775093d17a148843f8e9cfda47279f9
SacleuxBenoit <benoitsacleux@outlook.fr>
2020-05-09 17:39:50 +0200

    add template of Button Sizing in the component ButtonSizing.vue

---
+++ front/src/components/Button/ButtonSizing.vue
@@ -0,0 +1,54 @@
+<template>
+  <v-container fluid class="pa-0">
+    <v-row align="center">
+      <v-col cols="12" sm="6">
+        <div class="text-center">
+          <div class="my-2">
+            <v-btn x-small dark>Extra small Button</v-btn>
+          </div>
+          <div class="my-2">
+            <v-btn small dark>Small Button</v-btn>
+          </div>
+          <div class="my-2">
+            <v-btn dark>Normal Button</v-btn>
+          </div>
+          <div class="my-2">
+            <v-btn dark large>Large Button</v-btn>
+          </div>
+          <div class="my-2">
+            <v-btn x-large dark>Extra large Button</v-btn>
+          </div>
+        </div>
+      </v-col>
+      <v-col cols="12" sm="6">
+        <div class="text-center">
+          <div class="my-2">
+            <v-btn fab x-small dark>
+              <v-icon>mdi-television</v-icon>
+            </v-btn>
+          </div>
+          <div class="my-2">
+            <v-btn fab small dark>
+              <v-icon>mdi-pencil</v-icon>
+            </v-btn>
+          </div>
+          <div class="my-2">
+            <v-btn fab dark>
+              <v-icon>mdi-account-circle</v-icon>
+            </v-btn>
+          </div>
+          <div class="my-2">
+            <v-btn fab large dark>
+              <v-icon>mdi-alarm</v-icon>
+            </v-btn>
+          </div>
+          <div class="my-2">
+            <v-btn fab x-large dark>
+              <v-icon>mdi-domain</v-icon>
+            </v-btn>
+          </div>
+        </div>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
