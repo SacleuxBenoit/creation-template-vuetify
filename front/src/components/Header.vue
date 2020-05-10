dbf8a4fa130f4e0d1d5b621bf5638ceae4b1aa25
SacleuxBenoit <benoitsacleux@outlook.fr>
2020-05-10 17:29:18 +0200

    add a router-link for the page Footer in the component Header.vue

--- front/src/components/Header.vue
+++ front/src/components/Header.vue
@@ -2,19 +2,25 @@
   <v-card class="overflow-hidden">
     <div class="center HeaderColor">
       <v-toolbar-title>Template vuetify</v-toolbar-title> 
-       <router-link to="/">
-     <v-btn>
+
+      <router-link to="/">
+    <v-btn>
        <strong>Home</strong>
     </v-btn>
-       </router-link>
-       <router-link to="/button">
-    <v-btn>
-      <strong>Button</strong>
-    </v-btn>
-        </router-link> 
-    <v-btn>
- <router-link to="#"><strong>?</strong></router-link>
-    </v-btn>
+      </router-link>
+
+      <router-link to="/button">
+        <v-btn>
+          <strong>Button</strong>
+        </v-btn>
+      </router-link> 
+
+      <router-link to="/footer">
+        <v-btn>
+          <strong>Footer</strong>
+        </v-btn>
+      </router-link>
+      
   </div>
   </v-card>
 </template>
