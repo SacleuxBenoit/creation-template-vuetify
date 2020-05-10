634371f0b63a99ee83339014c320198bc8789923
SacleuxBenoit <benoitsacleux@outlook.fr>
2020-05-10 17:22:05 +0200

    delete the import of the component Footer in App.vue

--- front/src/App.vue
+++ front/src/App.vue
@@ -2,18 +2,15 @@
   <div>
     <Header/>
   <router-view />
-  <Footer/>
   </div>
 </template>
 
 <script>
 import Header from './components/Header'
-import Footer from './components/Footer'
 export default {
   name: "App",
   components:{
     Header,
-    Footer
   }
 };
 </script>
