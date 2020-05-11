9e34155b73687eb2b8eed2d8a11be605eb25d2d7
SacleuxBenoit <benoitsacleux@outlook.fr>
2020-05-11 16:16:52 +0200

    add template of a Footer in the component Footer.vue

---
+++ front/src/components/Footer/Footer.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-footer padless class="marginFooter">
+    <v-col
+      class="containerFooter"
+      cols="12">
+      {{ new Date().getFullYear() }} — <strong>Template vuetify</strong>
+    </v-col>
+  </v-footer>
+</template>
+
+<style>
+    .containerFooter{
+        border-bottom : 1px solid #000; 
+        text-align:center;
+}
+    .marginFooter{
+        margin-top: 2rem;
+}
+</style>
