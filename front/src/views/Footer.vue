d1cab0b67bc21e5391e5a97c7acc917fb60b8e98
SacleuxBenoit <benoitsacleux@outlook.fr>
2020-05-11 17:31:04 +0200

    import the component Footer2 in views/Footer

--- front/src/views/Footer.vue
+++ front/src/views/Footer.vue
@@ -1,14 +1,17 @@
 <template>
     <div>
         <Footer/>
+        <Footer2/>
     </div>
 </template>
 
 <script>
 import Footer from '../components/Footer/Footer'
+import Footer2 from '../components/Footer/Footer2'
 export default {
     components:{
-        Footer
+        Footer,
+        Footer2
     }
 }
 </script>
