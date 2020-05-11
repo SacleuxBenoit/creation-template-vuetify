8818103250b5e3d998f8a11ce1af5c08b3c64ff8
SacleuxBenoit <benoitsacleux@outlook.fr>
2020-05-11 17:30:28 +0200

    add template of a Footer in the component Footer2.vue

---
+++ front/src/components/Footer/Footer2.vue
@@ -0,0 +1,43 @@
+
+<template >
+  <v-footer
+    padless
+  >
+    <v-row
+      justify="center"
+      no-gutters
+    >
+      <v-btn 
+        v-for="link in links"
+        :key="link"
+        text
+      >
+        {{ link }}
+      </v-btn>
+      <v-col
+        cols="12"
+      >
+        {{ new Date().getFullYear() }} — <strong>Template vuetify</strong>
+      </v-col>
+    </v-row>
+  </v-footer>
+</template>
+
+<script>
+  export default {
+    data: () => ({
+      links: [
+        'Home',
+        'About Us',
+        'Team',
+        'Services',
+        'Blog',
+        'Contact Us',
+      ],
+    }),
+  }
+</script>
+
+<style>
+
+</style>
