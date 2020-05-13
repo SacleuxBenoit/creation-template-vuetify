3c3c70b8b928789f9475e6d8f200c774138d87fa
SacleuxBenoit <benoitsacleux@outlook.fr>
2020-05-13 16:03:09 +0200

    add import of the component Card.vue in views/Card.vue

--- front/src/views/Card.vue
+++ front/src/views/Card.vue
@@ -1,13 +1,14 @@
 <template>
     <div>
-        
+        <Card/>
     </div>
 </template>
 
 <script>
+import Card from '../components/Card/Card'
 export default {
     components:{
-        
+        Card,
     }
 }
 </script>
