aacd0980a0eebb59f862cc658245cbfd2df40a71
SacleuxBenoit <benoitsacleux@outlook.fr>
2020-05-14 23:34:58 +0200

    add the template of a card in the component Card.vue

---
+++ front/src/components/Card/Card.vue
@@ -0,0 +1,41 @@
+<template>
+  <v-card>
+    <v-container>
+      <v-row justify="space-between">
+        <v-col cols="auto">
+          <v-img
+            height="200"
+            width="200"
+            src="https://cdn.vuetifyjs.com/images/cards/store.jpg"
+          ></v-img>
+        </v-col>
+        <v-col
+          cols="auto"
+        >
+          <v-row
+            justify="center"
+          >
+
+            <v-col class="px-0">
+              <v-btn icon>
+                <v-icon>mdi-heart</v-icon>
+              </v-btn>
+            </v-col>
+
+            <v-col class="px-0">
+              <v-btn icon>
+                <v-icon>mdi-bookmark</v-icon>
+              </v-btn>
+            </v-col>
+
+            <v-col class="px-0">
+              <v-btn icon>
+                <v-icon>mdi-share-variant</v-icon>
+              </v-btn>
+            </v-col>
+          </v-row>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-card>
+</template>
